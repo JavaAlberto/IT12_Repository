c458874a0b1f017f3b0b3431d3a82b650e432e5c
Albrecht Pfleiderer <Albrecht.Pfleiderer@berufsschule-freising.de>
2012-09-15 16:26:59 +0200

    Zweiter Commit : Ausgabe modifiziert

--- IT12_AEP1/src/Testklasse.java
+++ IT12_AEP1/src/Testklasse.java
@@ -5,6 +5,7 @@ public class Testklasse
 	public static void main(String[] args) 
 	{
 	 System.out.println("Hier wird die Funktionsweise von EGit getestet!");
+	 System.out.println("Hier wurde der Dateiinhalt verändert! ");
 	}
 
 }
